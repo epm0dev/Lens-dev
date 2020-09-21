811a81a3f78a44609143bfd9a07d510315862889
epmancini <epmancini@live.com>
2020-09-21 18:38:31 -0400

    Fix some issues with project page animations, add box shadow overlay for non-focused project cards, add automatic reording of project cards based on which project's details are being displayed (if any), update README.md

--- static/javascript/projects.js
+++ static/javascript/projects.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
     // Store the computed style of the document.
     let style = getComputedStyle(document.body);
 
-
     // Iterate through each DOM element with the 'project-card-status-badge' class.
     $(".status-badge").each(function(index, element) {
         // Store the current element's text contents.
@@ -35,10 +34,13 @@ $(document).ready(function () {
         $(element).css('background-color', color);
     });
 
-    // TODO Description ...
-    $('#detail-column').data('currentProjectId', -1);
+    // TODO Comments ...
+    let detailColumn = $('#detail-column').data('currentProjectId', -1);
 
-    // TODO Description ...
+    // TODO Comments ...
+    let shadowBox = $('#shadow-box').height($(document).height());
+
+    // TODO Comments ...
     $('.see-more-text').each(function (index, element) {
         $(element).addClass('not-hovering')
         $(element).mouseenter(function () {
@@ -46,31 +48,47 @@ $(document).ready(function () {
         }).mouseleave(function () {
             $(element).addClass('not-hovering').removeClass('hovering');
         }).click(function () {
-            let projectId = $(element).parentsUntil('.project-card').parent().data('projectId');
-            let detailColumn = $('#detail-column');
-            let currentProjectId = detailColumn.data('currentProjectId');
-
-            detailColumn.data('currentProjectId', projectId);
             detailColumn.addClass('expanded').removeClass('collapsed');
 
-            setTimeout(function () {
+            let projectCard = $(element).parent().parent().parent();
+            $(".order-first").removeClass('order-first');
+            projectCard.addClass('order-first');
+
+            let projectId = projectCard.data('projectId');
+            let currentProjectId = detailColumn.data('currentProjectId');
+
+            if (projectId !== currentProjectId) {
                 populateDetailColumn(projectId);
-            }, 400)
+            }
+
+            detailColumn.data('currentProjectId', projectId);
         });
     });
 
-    // TODO Description ...
+    // TODO Comments ...
     $('.exit-button').click(function () {
             $('.detail-container').addClass('hide').removeClass('show');
             setTimeout(function () {
-                $('#detail-column').addClass('collapsed').removeClass('expanded');
-            }, 400)
+                $('.detail-container').addClass('no-display');
+                shadowBox.addClass('hide-shadow').removeClass('show-shadow');
+                detailColumn.addClass('collapsed').removeClass('expanded');
+                detailColumn.data('currentProjectId', -1);
+                $(".order-first").removeClass('order-first');
+            }, 300);
         });
 
+    // TODO Comments ...
     function populateDetailColumn(projectId) {
-        $('.show').addClass('hide').removeClass('show');
-        $('.detail-container').filter(function (index, element) {
-            return $(element).data('projectId') === projectId;
-        }).addClass('show').removeClass('hide');
+        let current = $('.show').addClass('hide').removeClass('show');
+        shadowBox.addClass('show-shadow').removeClass('hide-shadow');
+        setTimeout(function () {
+            current.addClass('no-display');
+            let container = $('.detail-container').filter(function (index, element) {
+                return $(element).data('projectId') === projectId;
+            }).removeClass('no-display');
+            setTimeout(function () {
+                container.addClass('show').removeClass('hide');
+                }, 200);
+            }, 400);
     }
 });
